62f8de04905bd15d1113b6460d31347acc8c6b0e
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-05-21 15:59:47 +0300

    feat: actions

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,11 +1,15 @@
 import {getInitialData} from "../utils/api";
 import {receiveUsers} from "./users";
 import {recievedTweets} from "./tweets";
+import {setAuthedUSer} from "../actions/authedUser";
+
+const AUTHED_ID = "tylermcginnis";
 export const handleInitialData = () => {
     return (dispatch) => {
         return getInitialData().then(({users, tweets}) => {
             dispatch(receiveUsers(users));
             dispatch(recievedTweets(tweets));
+            dispatch(setAuthedUSer(AUTHED_ID));
         });
     };
 };
